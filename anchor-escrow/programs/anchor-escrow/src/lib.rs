ee52ee11b7d05e112fc02b25c9cf371b1fd0e32a
Rudra Prasad <rudraofficial202@gmail.com>
2025-11-03 23:09:42 +0530

    escrow with instructions

--- anchor-escrow/programs/anchor-escrow/src/lib.rs
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -1,13 +1,26 @@
 use anchor_lang::prelude::*;
 
+pub mod instructions;
+pub mod state;
+
+use instructions::*;
+use state::*;
 declare_id!("GHZ29nUjyQrLcfxFNUTtKbLxmrkW3Za7ysk9Xi6yofsc");
 
 #[program]
 pub mod anchor_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Make>, seed: u64, deposit: u64, recive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, recive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+
+        Ok(())
+    }
+    pub fn take(ctx: Context<Make>, seed: u64, deposit: u64, recive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, recive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+
         Ok(())
     }
 }
